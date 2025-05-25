07f4110a48df4f263aeac608c4f5c70bdaa48dbc
Arthur Quinellato <arthur.quinellato@gmail.com>
2025-05-25 16:46:43 -0300

    Try to fix issues with the flash messages (with no success)

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -4,7 +4,16 @@ class RegistrationController < ApplicationController
   end
 
   def create
-    params
-    params[:user]
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: "Successfully created account"
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
